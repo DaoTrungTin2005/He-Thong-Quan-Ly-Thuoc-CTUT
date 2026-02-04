1350e118a3ac8aa8d65a83696001fcece8964c23
Tin Dao <trungtin07012005@gmail.com>
2026-02-04 10:34:36 +0700

    Create Project with Gradle

---
+++ BE/src/test/java/student/ctuet/edu/vn/pharmacy_management/PharmacymanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package student.ctuet.edu.vn.pharmacy_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacymanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
