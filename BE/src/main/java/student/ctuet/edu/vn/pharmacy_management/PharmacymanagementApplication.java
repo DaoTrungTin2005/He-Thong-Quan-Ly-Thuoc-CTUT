56ce93fc44f29af1edd972851b3c62fe4d78785e
Tin Dao <trungtin07012005@gmail.com>
2026-02-04 10:57:22 +0700

    Creating Spring Project

---
+++ BE/src/main/java/student/ctuet/edu/vn/pharmacy_management/PharmacymanagementApplication.java
@@ -0,0 +1,13 @@
+package student.ctuet.edu.vn.pharmacy_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacymanagementApplication.class, args);
+	}
+
+}
