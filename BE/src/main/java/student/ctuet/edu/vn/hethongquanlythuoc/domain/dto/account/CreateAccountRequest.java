4cbd7b56b63f3dd20eac7f54269b403b03d383ee
Tin Dao <trungtin07012005@gmail.com>
2026-02-07 20:22:34 +0700

    Change to record for CreateAccountRequest

--- BE/src/main/java/student/ctuet/edu/vn/hethongquanlythuoc/domain/dto/account/CreateAccountRequest.java
+++ BE/src/main/java/student/ctuet/edu/vn/hethongquanlythuoc/domain/dto/account/CreateAccountRequest.java
@@ -3,20 +3,17 @@ package student.ctuet.edu.vn.hethongquanlythuoc.domain.dto.account;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public class CreateAccountRequest {
-    @NotBlank(message = "Tên người dùng không được để trống")
-    String ten_nguoi_dung;
+public record CreateAccountRequest(
 
-    @NotBlank(message = "Email không được để trống")
-    @Email(message = "Email không hợp lệ")
-    String email;
+        @NotBlank(message = "Tên người dùng không được để trống") String fullname,
 
-    @NotBlank(message = "Tên đăng nhập không được để trống")
-    String ten_dang_nhap;
+        @NotBlank(message = "Email không được để trống") @Email(message = "Email không hợp lệ") String email,
 
-    @NotBlank(message = "Mật khẩu không được để trống")
-    String mat_khau;
+        @NotBlank(message = "Tên đăng nhập không được để trống") String username,
 
-    @NotBlank(message = "Vui lòng chọn vai trò")
-    String vai_tro;
+        @NotBlank(message = "Mật khẩu không được để trống") String password,
+
+        @NotBlank(message = "Vui lòng chọn vai trò") String role
+
+) {
 }
