f23324d2bde6db1f8b9a4414ee3aff0765973c39
Tin Dao <trungtin07012005@gmail.com>
2026-02-25 16:31:30 +0700

    config(security): add HttpMethod.POST for "/api/v1/accounts"

--- BE/src/main/java/student/ctuet/edu/vn/hethongquanlythuoc/config/SecurityConfig.java
+++ BE/src/main/java/student/ctuet/edu/vn/hethongquanlythuoc/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -35,7 +36,7 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/api/v1/accounts")
+                                HttpMethod.POST, "/api/v1/accounts")
                         .permitAll()
                         .anyRequest().authenticated());
 
