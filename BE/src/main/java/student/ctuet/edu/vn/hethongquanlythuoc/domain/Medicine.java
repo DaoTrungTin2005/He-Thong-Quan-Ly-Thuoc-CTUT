d7c4a50fb96f62407affaf7419a708ac3331a4e0
Tin Dao <trungtin07012005@gmail.com>
2026-03-05 22:46:08 +0700

    feat(medicine): implement MedicineResponse and CreateMedicineRequest

--- BE/src/main/java/student/ctuet/edu/vn/hethongquanlythuoc/domain/Medicine.java
+++ BE/src/main/java/student/ctuet/edu/vn/hethongquanlythuoc/domain/Medicine.java
@@ -2,10 +2,8 @@ package student.ctuet.edu.vn.hethongquanlythuoc.domain;
 
 import java.time.Instant;
 import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -35,7 +33,7 @@ public class Medicine {
     private String unit;
 
     @ManyToOne
-    @JoinColumn(name = "status_id", nullable = false)
+    @JoinColumn(name = "ma_trang_thai_thuoc", nullable = false)
     private MedicineStatus status;
 
     @OneToMany(mappedBy = "medicine")
@@ -50,7 +48,7 @@ public class Medicine {
     @PrePersist
     protected void onCreate() {
         createdAt = Instant.now();
-        updatedAt = Instant.now();
+        updatedAt = Instant.now();  
     }
 
     @PreUpdate
