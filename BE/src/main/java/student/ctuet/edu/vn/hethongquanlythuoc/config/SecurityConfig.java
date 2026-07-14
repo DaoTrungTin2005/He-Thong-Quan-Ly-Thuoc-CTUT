4e3f59d6953dd743274ca268ee829c1d95f39fbd
Tin Dao <trungtin07012005@gmail.com>
2026-07-14 15:01:41 +0700

    Add CORS

--- BE/src/main/java/student/ctuet/edu/vn/hethongquanlythuoc/config/SecurityConfig.java
+++ BE/src/main/java/student/ctuet/edu/vn/hethongquanlythuoc/config/SecurityConfig.java
@@ -105,12 +105,12 @@ public class SecurityConfig {
         @Bean
         public CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(List.of("http://localhost:5173", "https://he-thong-quan-ly-thuoc-ctut-tocr.vercel.app"));
+                config.setAllowedOrigins(List.of("http://localhost:5173", "https://he-thong-quan-ly-thuoc-ctut-tocr.vercel.app", "tauri://localhost", "https://tauri.localhost", " http://tauri.localhost"  ));
                 config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
                 config.setAllowedHeaders(List.of("*"));
                 config.setAllowCredentials(true);
                 UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-                source.registerCorsConfiguration("/**", config); // Applies CORS configuration to all endpoints
+                source.registerCorsConfiguration("/**", config); 
                 return source;
         }
 }
